3887de79159a003a160a18fda84858db7af80951
kalouo <omar@zipmex.com>
2021-08-09 12:28:08 +0200

    feat: update_administrator and tests for Vault

--- contracts/vault.py
+++ contracts/vault.py
@@ -2,24 +2,41 @@ import smartpy as sp
 
 from contracts.utils import Utils
 
+class ErrorMessage: 
+    NOT_ADMIN = "{}NOT_ADMIN"
+
 
 class Vault(sp.Contract):
-    def __init__(self):
+    def __init__(self, administrator):
         self.init_type(sp.TRecord(administrator=sp.TAddress))
+        self.init(**self.get_initial_storage(administrator))
+
 
     @sp.entry_point
-    def deposit(self, asset_contract_address, asset_token_id):
-        sp.set_type(asset_contract_address, sp.TAddress)
-        sp.set_type(asset_token_id, sp.TNat)
+    def update_administrator(self, new_administrator): 
+          sp.set_type(new_administrator, sp.TAddress)
+          sp.verify(sp.sender == self.data.administrator, message=ErrorMessage.NOT_ADMIN)
+          self.data.administrator = new_administrator
 
-        Utils.execute_token_transfer(
-            asset_contract_address, sp.sender, sp.self_address, asset_token_id, sp.nat(1))
-
-
+    def get_initial_storage(self, administrator):
+        storage = {}
+        storage['administrator'] = administrator
+        return storage
+        
 @sp.add_test(name="Vault")
 def test():
-    pass
+    scenario = sp.test_scenario()
+    administrator = sp.test_account("Administrator")
+    alice = sp.test_account("Alice")
 
+    vault = Vault(administrator.address)
+    scenario += vault
 
-sp.add_compilation_target("vault", Vault(), storage=sp.record(
-    administrator=sp.address("tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU")))
+    scenario.verify(vault.data.administrator == administrator.address)
+    
+    scenario += vault.update_administrator(alice.address).run(sender=alice.address, valid = False, exception=ErrorMessage.NOT_ADMIN)
+    scenario += vault.update_administrator(alice.address).run(sender=administrator.address)
+
+    scenario.verify(vault.data.administrator == alice.address)
+
+sp.add_compilation_target("vault", Vault(sp.address("tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU")))
